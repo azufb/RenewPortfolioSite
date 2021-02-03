9ec0eb3b9f1ba1280407349deb9716267da1be8a
Azusa Okamoto <curiousazunas@gmail.com>
2021-02-04 00:02:57 +0900

    自己紹介ページの作成とプロダクトページの作成

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ document.write(
     '\t\t<ul class="navi">',
         '\t\t\t<li><a href="index.html">TOP</a> | </li>',
         '\t\t\t<li><a href="introductionTop.html">自己紹介</a> | </li>',
-        '\t\t\t<li><a href="./column/2103.html">ミニコラム</a> | </li>',
+        '\t\t\t<li><a href="./products/productsList.html">成果物</a> | </li>',
         '\t\t\t<li><a href="contacts.html">お問い合わせ</a></li>',
     '\t\t</ul>',
     '\t</nav>',
---
+++ products/header.js
@@ -0,0 +1,13 @@
+document.write(
+    '<div class="top">',
+    '\t<h1><a href="./../index.html">あずなすの開発ラボ</a></h1>',
+    '\t<nav>',
+    '\t\t<ul class="navi">',
+        '\t\t\t<li><a href="./../index.html">TOP</a> | </li>',
+        '\t\t\t<li><a href="./../introductionTop.html">自己紹介</a> | </li>',
+        '\t\t\t<li><a href="productsList.html">成果物</a> | </li>',
+        '\t\t\t<li><a href="./../contacts.html">お問い合わせ</a></li>',
+    '\t\t</ul>',
+    '\t</nav>',
+    '</div>'
+)
